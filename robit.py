689bf2f885fa7aba945d23563b800561180513ea
F13 <F13@users.noreply.github.com>
2023-09-29 21:29:24 -0500

    Status messages

--- robit.py
+++ robit.py
@@ -3,13 +3,16 @@
 from helpers import AIHelper, AudioHelper
 import tempfile, os
 
-ai = AIHelper.AIHelper()
+ai = AIHelper.AIHelper(tts_endpoint="http://192.168.1.50:59125/api/tts")
 
 def process_audio(file):
+    print("Transcription beginning...")
     user_message = ai.transcribe(file)
     print(user_message)
+    print("Thinking...")
     bot_message = ai.chat(user_message)
     print(bot_message)
+    print("Audioifying...")
     wav = ai.text_to_wav(bot_message, {"lengthScale":"0.75"})
     AudioHelper.AudioHelper().say(wav)
 
@@ -17,6 +20,7 @@ if __name__ == "__main__":
     while True:
         try:
             tf = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+            print("Listening...")
             AudioHelper.AudioHelper(dst_filename=tf.name).listen()
             process_audio(tf.name)
             tf.close()
